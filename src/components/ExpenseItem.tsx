22800e6fc39407ca0f0859181571857caa94d096
IbrahimMuharib <ibrahim.a.muharib@gmail.com>
2023-07-15 00:34:58 +0300

    fix: use deconstructed props

--- src/components/ExpenseItem.tsx
+++ src/components/ExpenseItem.tsx
@@ -1,12 +1,12 @@
 import "./ExpenseItem.css";
 import Expense from "./Expense";
-function ExpenseItem(props: { expense: Expense }): JSX.Element | null {
+function ExpenseItem({ expense }: { expense: Expense }): JSX.Element | null {
   return (
     <div className="expense-item">
-      <h3>{props.expense.date.toString()}</h3>
+      <h3>{expense.date.toString()}</h3>
       <div className="expense-item__description">
-        <h2>{props.expense.title}</h2>
-        <div className="expense-item__price">€ {props.expense.amount}</div>
+        <h2>{expense.title}</h2>
+        <div className="expense-item__price">€ {expense.amount}</div>
       </div>
     </div>
   );
