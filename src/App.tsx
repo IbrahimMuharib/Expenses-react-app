f748f1fa3a1fe5ea977adc62e89aa37834f27ce1
IbrahimMuharib <ibrahim.a.muharib@gmail.com>
2023-07-18 08:00:53 +0300

    feat: change title on enter

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,68 @@
-import { Expense, RawExpense, TitleChangeInfo } from "./components/ExpensesList/Expense.ts";
+import {
+  Expense,
+  RawExpense,
+  TitleChangeInfo,
+} from "./components/ExpensesList/Expense.ts";
 import Expenses from "./components/ExpensesList/Expenses/Expenses";
 import "./App.css";
 import Card from "./components/UI/Card";
 import NewExpense from "./components/CreateExpenses/NewExpense/NewExpense.tsx";
-import {parseJsonData, getLocalStorageData} from "./utils/parser.tsx";
-import { useState } from 'react';
+import { parseJsonData, getLocalStorageData } from "./utils/parser.tsx";
+import { useState } from "react";
 
 function App(): JSX.Element | null {
-
-  function addExpenseHander(rawExpense:RawExpense){
-    let rawExpensesList : RawExpense[] = getLocalStorageData()
-    rawExpensesList.push(rawExpense)
-    localStorage.setItem('Expenses', JSON.stringify(rawExpensesList));
-    const newExpensesList:Expense[] = parseJsonData(rawExpensesList)
-    setExpensesList(newExpensesList)
-    setFilteredYear('All')
+  function addExpenseHander(rawExpense: RawExpense) {
+    let rawExpensesList: RawExpense[] = getLocalStorageData();
+    rawExpensesList.push(rawExpense);
+    localStorage.setItem("Expenses", JSON.stringify(rawExpensesList));
+    const newExpensesList: Expense[] = parseJsonData(rawExpensesList);
+    setExpensesList(newExpensesList);
+    setFilteredYear("All");
   }
 
-  function changeTitleHandler(titleChangeInfo:TitleChangeInfo){
-    let rawExpensesList : RawExpense[] = getLocalStorageData()
-    const findIndex = rawExpensesList.find(rawExpense => {
+  function changeTitleHandler(titleChangeInfo: TitleChangeInfo) {
+    let rawExpensesList: RawExpense[] = getLocalStorageData();
+    const findIndex = rawExpensesList.find((rawExpense) => {
       return rawExpense.id === titleChangeInfo.id;
-     });
-     if (findIndex !== undefined) {
+    });
+    if (findIndex !== undefined) {
       findIndex.title = titleChangeInfo.enteredTitle;
-     }
-    localStorage.setItem('Expenses', JSON.stringify(rawExpensesList));
-    const newExpensesList:Expense[] = parseJsonData(rawExpensesList)
-    setExpensesList(newExpensesList)
-  }
-
-  function filterYear(year:string){
-    let rawExpensesList : RawExpense[] = getLocalStorageData()
-    let newExpensesList:Expense[] = parseJsonData(rawExpensesList)
-    if (year === "All"){
-      setExpensesList(newExpensesList)
-      return
     }
-    newExpensesList = newExpensesList.filter((item)=>{
-      console.log(item.date.getFullYear().toString())
-      return item.date.getFullYear().toString() === year;
-    })
-    setExpensesList(newExpensesList)    
+    localStorage.setItem("Expenses", JSON.stringify(rawExpensesList));
+    const newExpensesList: Expense[] = parseJsonData(rawExpensesList);
+    setExpensesList(newExpensesList);
   }
 
-  let rawExpensesList : RawExpense[] = getLocalStorageData()
-  const [expensesList, setExpensesList] = useState<Expense[]>(parseJsonData(rawExpensesList))
-  const [filteredYear, setFilteredYear] = useState<string>('All')
+  function filterYear(year: string) {
+    let rawExpensesList: RawExpense[] = getLocalStorageData();
+    let newExpensesList: Expense[] = parseJsonData(rawExpensesList);
+    if (year === "All") {
+      setExpensesList(newExpensesList);
+      return;
+    }
+    newExpensesList = newExpensesList.filter((item) => {
+      return item.date.getFullYear().toString() === year;
+    });
+    setExpensesList(newExpensesList);
+  }
+
+  let rawExpensesList: RawExpense[] = getLocalStorageData();
+  const [expensesList, setExpensesList] = useState<Expense[]>(
+    parseJsonData(rawExpensesList)
+  );
+  const [filteredYear, setFilteredYear] = useState<string>("All");
   return (
     <div>
       <Card className="expenses_header">
-        <NewExpense onAddExpense={addExpenseHander}/>
+        <NewExpense onAddExpense={addExpenseHander} />
       </Card>
-      <Expenses onchangeTitle= {changeTitleHandler} onFilterExpensesByYear = {filterYear} expenses={expensesList} filteredYear={filteredYear} setFilteredYear ={setFilteredYear}/>
+      <Expenses
+        onchangeTitle={changeTitleHandler}
+        onFilterExpensesByYear={filterYear}
+        expenses={expensesList}
+        filteredYear={filteredYear}
+        setFilteredYear={setFilteredYear}
+      />
     </div>
   );
 }
--- src/components/ExpensesList/ExpenseItem/ExpenseItem.tsx
+++ src/components/ExpensesList/ExpenseItem/ExpenseItem.tsx
@@ -1,22 +1,45 @@
 import "./ExpenseItem.css";
-import { Expense, TitleChangeInfo} from "../Expense";
+import { Expense, TitleChangeInfo } from "../Expense";
 import ExpenseDate from "../ExpenseDate/ExpenseDate";
 import Card from "../../UI/Card";
 import { useState } from "react";
-function ExpenseItem({ expense, onSaveNewTitle }: { expense: Expense, onSaveNewTitle:Function }): JSX.Element | null {
-  
-  const [title, setTitle] = useState(expense.title);
-  const [titleChangeInfo, setTitleChangeInfo] = useState<TitleChangeInfo>({enteredTitle:"", id:expense.id});
-  const clickHandler = () => {
-    setTitle(titleChangeInfo.enteredTitle)
-    onSaveNewTitle(titleChangeInfo)
-    setTitleChangeInfo((prevInfo)=>{return {...prevInfo, enteredTitle:""}})
-  }
+import React, { KeyboardEvent } from "react";
 
-  const changeHandler = (value:string, id:string) => {
-    setTitleChangeInfo({enteredTitle:value, id:id})
-  }
-  
+function ExpenseItem({
+  expense,
+  onSaveNewTitle,
+}: {
+  expense: Expense;
+  onSaveNewTitle: Function;
+}): JSX.Element | null {
+  const [title, setTitle] = useState(expense.title);
+  const [titleChangeInfo, setTitleChangeInfo] = useState<TitleChangeInfo>({
+    enteredTitle: "",
+    id: expense.id,
+  });
+
+  let enteredTitleRef = React.createRef();
+
+  const clickHandler = () => {
+    setTitle(titleChangeInfo.enteredTitle);
+    onSaveNewTitle(titleChangeInfo);
+    setTitleChangeInfo((prevInfo) => {
+      return { ...prevInfo, enteredTitle: "" };
+    });
+  };
+
+  const changeHandler = (value: string, id: string) => {
+    setTitleChangeInfo({ enteredTitle: value, id: id });
+  };
+
+  const handleKeypress = (event: KeyboardEvent<HTMLInputElement>) => {
+    //it triggers by pressing the enter key
+    if (!event.repeat && event.key === "Enter") {
+      clickHandler();
+      event.currentTarget.blur();
+    }
+  };
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={expense.date} />
@@ -25,7 +48,14 @@ function ExpenseItem({ expense, onSaveNewTitle }: { expense: Expense, onSaveNewT
         <Card className="expense-item__price">€ {expense.amount}</Card>
       </div>
       <button onClick={clickHandler}> Change Title </button>
-      <input type="text" value={titleChangeInfo.enteredTitle} onChange={(event)=>{changeHandler(event.target.value, expense.id)}}></input>
+      <input
+        type="text"
+        value={titleChangeInfo.enteredTitle}
+        onChange={(event) => {
+          changeHandler(event.target.value, expense.id);
+        }}
+        onKeyUp={handleKeypress}
+      ></input>
     </Card>
   );
 }
